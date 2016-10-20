264bbdf254804219635334a5387d446f893adaeb
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-10-20 16:22:23 +0200

    Add Muli interface/methods to default compiler classpath

--- src/java/de/wwu/muli/BackendMain.java
+++ src/java/de/wwu/muli/BackendMain.java
@@ -2,13 +2,24 @@ package de.wwu.muli;
 
 import org.extendj.JavaCompiler;
 
+import java.util.Arrays;
+
 public class BackendMain extends JavaCompiler {
     public BackendMain() {
         super("muli-lang");
     }
 
     public static void main(String args[]) {
-        int exitCode = new BackendMain().run(args);
+        // Add Muli classpath to compiler classpath
+        final String augmentedArgs[] = new String[args.length+2];
+        System.arraycopy(args, 0, augmentedArgs, 0, args.length);
+        augmentedArgs[args.length] = "-classpath";
+        augmentedArgs[args.length+1] = "../muli-cp/build/classes/main/"; // TODO add a reliable (absolute?) path here
+
+        // TODO actually patch args: look for existing -classpath and add new path together with File.pathSeparator.
+
+        // Begin backend execution
+        int exitCode = new BackendMain().run(augmentedArgs);
         if (exitCode != 0) {
             System.exit(exitCode);
         }
