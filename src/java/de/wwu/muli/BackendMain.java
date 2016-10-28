6a9686871caaae3c669d5c29abbf6ad3bb7cc772
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-10-28 13:56:45 +0200

    Add mulicp to classpath and make it accessible by extendj

--- src/java/de/wwu/muli/BackendMain.java
+++ src/java/de/wwu/muli/BackendMain.java
@@ -1,8 +1,11 @@
 package de.wwu.muli;
 
 import org.extendj.JavaCompiler;
-
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class BackendMain extends JavaCompiler {
     public BackendMain() {
@@ -10,11 +13,19 @@ public class BackendMain extends JavaCompiler {
     }
 
     public static void main(String args[]) {
+        String subClassPath;
+        try {
+            // Export library (mulicp) to disk
+            subClassPath = exportSubClassPath();
+        } catch (IOException e) {
+            throw new RuntimeException("Error: Could not export the required class path files to the filesystem. Aborting.", e);
+        }
+
         // Add Muli classpath to compiler classpath
         final String augmentedArgs[] = new String[args.length+2];
         System.arraycopy(args, 0, augmentedArgs, 0, args.length);
         augmentedArgs[args.length] = "-classpath";
-        augmentedArgs[args.length+1] = "../muli-cp/build/classes/main/"; // TODO add a reliable (absolute?) path here
+        augmentedArgs[args.length+1] = subClassPath;
 
         // TODO actually patch args: look for existing -classpath and add new path together with File.pathSeparator.
 
@@ -22,6 +33,49 @@ public class BackendMain extends JavaCompiler {
         int exitCode = new BackendMain().run(augmentedArgs);
         if (exitCode != 0) {
             System.exit(exitCode);
+        }
+    }
+
+    private static String exportSubClassPath() throws IOException {
+        File dest = File.createTempFile("mulicp", ".jar");
+        dest.deleteOnExit();
+        exportResource("/mulicp.jar", dest);
+        return dest.toString();
+    }
+
+    /**
+     * Export a resource embedded into the Jar file to the local file path.
+     * Adapted from http://stackoverflow.com/a/13379744/96203
+     * (original author: Ordiel@Stackoverflow)
+     *
+     * @param fromResourceName e.g. "/SmartLibrary.dll"
+     * @param to Target File for output, created e.g. by File.createTempFile()
+     * @throws IOException if anything goes wrong
+     */
+    static private void exportResource(String fromResourceName, File to) throws IOException {
+        InputStream stream = null;
+        OutputStream resStreamOut = null;
+        try {
+            stream = BackendMain.class.getResourceAsStream(fromResourceName);
+            if(stream == null) {
+                throw new IOException("Cannot get resource \"" + fromResourceName + "\" from Jar file.");
+            }
+
+            int readBytes;
+            byte[] buffer = new byte[4096];
+            resStreamOut = new FileOutputStream(to);
+            while ((readBytes = stream.read(buffer)) > 0) {
+                resStreamOut.write(buffer, 0, readBytes);
+            }
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+            if (resStreamOut != null) {
+                resStreamOut.close();
+            }
         }
     }
 
